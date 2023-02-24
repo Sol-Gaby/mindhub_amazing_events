700002451732ec7ced460284736feababca05657
Soledad Adorno <gabriela.juj@gmail.com>
2023-02-23 22:45:31 -0300

    se creo pastEvents.html, main.js y data.js, falta arreglar las cards

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+console.log(data);
+const eventos = data.events;
+console.log(eventos);
+
+let eventCartas=[];
+
+function crearEvents(){
+    
+    for (let event of eventos){
+        let cartaEv = `<div class="card p-0 g-5 mx-2">
+        <img src="${event.image}" class="card-img-top img_fit" alt="cinema">
+            <div class="card-body fondo_cards">
+                <h5 class="card-title">${event.name}</h5>
+                <p class="card-text">${event.description}</p>
+                <a href="details.html" class="btn btn-outline-danger">See more</a>
+            </div>
+    </div>`
+    // todos los estilos que le quiera dar a las tarjetas lo tengo que aplicar aca AuthenticatorAssertionResponse, ya no se toca el html
+    eventCartas.push(cartaEv)
+    } 
+}
+console.log(eventCartas);
+
+function printEvents(){
+    let cartaEv = document.getElementById('cardEvents');
+    cartaEv.innerHTML=eventCartas.join('')
+}
+
+crearEvents();
+printEvents();
+
