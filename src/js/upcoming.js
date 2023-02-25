a2de4573eaec7fad9cac68a54ca0bde4e3d88c6c
Soledad Adorno <gabriela.juj@gmail.com>
2023-02-25 18:59:23 -0300

    upcoming.js creado y funcionando, siguen quedando para solucionar los estilos

---
+++ src/js/upcoming.js
@@ -0,0 +1,33 @@
+console.log(data);
+const eventos = data.events;
+console.log(eventos);
+
+let eventCartas = [];
+
+console.log(eventCartas);
+
+function printEvents()
+{
+    for (let event of eventos) {
+        if (event.date >= "2022") {
+            let cartaEv = `<div class="card carta_body">
+            <img src="${event.image}" class="card-img-top img_fit" alt="cinema">
+                <div class="card-body fondo_cards">
+                    <h5 class="card-title">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
+                    <a href="details.html" class="btn btn-outline-danger">See more</a>
+                </div>
+            </div>`
+            eventCartas.push(cartaEv)
+
+        }
+    }
+}
+function printSelected()
+{
+    let cartaEv = document.getElementById('cardEvents_up');
+    cartaEv.innerHTML = eventCartas.join('')
+}
+
+printEvents();
+printSelected();
