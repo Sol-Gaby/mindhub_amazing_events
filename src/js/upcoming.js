233eaca339ba74761cc0651221c9b72b65a700a4
Soledad Adorno <gabriela.juj@gmail.com>
2023-02-25 22:27:48 -0300

    agregue mx-3

--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -10,7 +10,7 @@ function printEvents()
 {
     for (let event of eventos) {
         if (event.date > "2021-12-31") {
-            let cartaEv = `<div class="card g-4 p-0 mx-1 carta_body">
+            let cartaEv = `<div class="card g-4 p-0 mx-3 carta_body">
             <img src="${event.image}" class="card-img-top img_fit" alt="cinema">
                 <div class="card-body fondo_cards">
                     <h5 class="card-title">${event.name}</h5>
