83e1f938a2d57b4ec2312fb6c9c66ac40dee41eb
Soledad Adorno <gabriela.juj@gmail.com>
2023-03-08 12:22:53 -0300

    los filtros funcionando en upcoming

--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -1,13 +1,30 @@
-console.log(data);
+// console.log(data);
 const eventos = data.events;
 console.log(eventos);
 
-let eventCartas = [];
-console.log(eventCartas);
+// console.log(eventCartas);
 
-function printEvents()
-{
-    for (let event of eventos) {
+// function printEvents()
+// {
+//     for (let event of eventos) {
+//         if (event.date > "2021-12-31") {
+//             let cartaEv = `<div class="card g-4 p-0 mx-3 carta_body">
+//             <img src="${event.image}" class="card-img-top img_fit" alt="cinema">
+//                 <div class="card-body fondo_cards">
+//                     <h5 class="card-title">${event.name}</h5>
+//                     <p class="card-text">${event.description}</p>
+//                     <a href="./details.html?id=${event._id}" class="btn btn-outline-danger mb-0">See more</a>
+//                 </div>
+//             </div>`
+//             eventCartas.push(cartaEv)
+
+//         }
+//     }
+// }
+
+function crearUpEv(arrayDeEventos){
+    let eventCartas = [];
+    for (let event of arrayDeEventos) {
         if (event.date > "2021-12-31") {
             let cartaEv = `<div class="card g-4 p-0 mx-3 carta_body">
             <img src="${event.image}" class="card-img-top img_fit" alt="cinema">
@@ -18,18 +35,27 @@ function printEvents()
                 </div>
             </div>`
             eventCartas.push(cartaEv)
-
         }
     }
+    return eventCartas;
 }
 
 
-function printSelected()
-{
-    let cartaEv = document.getElementById('cardEvents_up');
-    cartaEv.innerHTML = eventCartas.join('')
-}
-console.log(eventCartas);
+// function printSelected()
+// {
+//     let cartaEv = document.getElementById('cardEvents_up');
+//     cartaEv.innerHTML = eventCartas.join('')
+// }
+// console.log(eventCartas);
 
-printEvents();
-printSelected();
+// printEvents();
+// printSelected();
+
+function printEvents(array, x){
+    let cartaEv = document.getElementById(x);
+    let templates = crearUpEv(array) //templates va a guardar el array con todos los templates de cada card
+    cartaEv.innerHTML = templates.join('')
+}
+
+// crearEvents();
+printEvents(eventos, 'cardEvents_main');
