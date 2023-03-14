a89135a631038d750b3daabbcb2c50b873cafc57
Soledad Adorno <gabriela.juj@gmail.com>
2023-03-13 21:28:53 -0300

    se creo cartas.js, la idea es modularizar el codigo, en main.js se creo una funcion para traer la API

---
+++ src/js/cartas.js
@@ -0,0 +1,22 @@
+// creacion de una card general
+// const eventos = data.events;
+// console.log(eventos);
+
+// comentario para ver que onda 
+
+/* function crearEvents(arrayDeEventos){
+    let eventCartas=[]; //eventscartas va a ser el array con todos mis templ de cada evento
+    for (let event of arrayDeEventos){
+        let cartaEv = `<div class="card g-4 p-0 mx-3 carta_body">
+        <img src="${event.image}" class="card-img-top img_fit" alt="cinema">
+            <div class="card-body fondo_cards py-4">
+                <h5 class="card-title my-2">${event.name}</h5>
+                <p class="card-text my-4">${event.description}</p>
+                <a href="./details.html?id=${event._id}" class="btn btn-outline-danger boton">See more</a>
+            </div>
+    </div>`
+    eventCartas.push(cartaEv)
+    // todos los estilos que le quiera dar a las tarjetas lo tengo que aplicar aca AuthenticatorAssertionResponse, ya no se toca el html//
+} 
+return eventCartas; //retorno ese array de events 
+} */
